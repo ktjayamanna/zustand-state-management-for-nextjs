49ae708de3e3d5b40a5ce37db526cd40a27e8bbd
ktjayamanna <kaveen.jayamanna@gmail.com>
2023-12-02 15:31:57 -0500

    the app is pretty now and working

--- src/pages/color.js
+++ src/pages/color.js
@@ -7,11 +7,74 @@ export default function Color() {
   const { age } = router.query; // Retrieve age from the query parameters
   const [color, setColor] = useState("");
 
+  const containerStyle = {
+    paddingTop: "50px",
+    backgroundColor: "#F0F4F8", // light grey background
+    minHeight: "100vh",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+  };
+
+  const titleStyle = {
+    color: "#333333", // dark text for readability
+    marginBottom: "20px",
+  };
+
+  const selectStyle = {
+    width: "50%",
+    padding: "10px",
+    marginBottom: "200px",
+    borderRadius: "5px",
+    border: "1px solid #ced4da",
+    boxShadow: "inset 0 1px 2px rgba(0,0,0,.075)", // subtle inner shadow for depth
+    fontSize: "16px", // increase font size for better readability
+    cursor: "pointer",
+  };
+
+  const buttonStyle = {
+    padding: "10px 15px",
+    borderRadius: "5px",
+    boxShadow: "0 2px 4px rgba(0, 0, 0, 0.2)", // button shadow for depth
+    cursor: "pointer",
+    textDecoration: "none",
+    display: "block",
+    width: "calc(90% - 4px)", // accounting for the margin
+    margin: "2px",
+    textAlign: "center",
+    border: "none",
+  };
+
+  const backButtonStyle = {
+    ...buttonStyle,
+    backgroundColor: "#6c757d", // a standard bootstrap secondary button color
+    color: "#FFFFFF", // white text for contrast
+    marginRight: "4px", // to separate it from the next button
+  };
+
+  const nextButtonStyle = {
+    ...buttonStyle,
+    backgroundColor: "#007bff", // a standard bootstrap primary button color
+    color: "#FFFFFF", // white text for contrast
+    marginLeft: "4px", // to separate it from the back button
+  };
+
+  const buttonContainerStyle = {
+    display: "flex",
+    justifyContent: "center", // This will center the buttons
+    alignItems: "center", // This will vertically align the buttons in case they have different heights
+    width: "100%",
+    maxWidth: "500px",
+    marginTop: "20px", // Adds some space above the buttons
+  };
+
+  // Inside your component's return statement:
   return (
-    <div className="container">
-      <h1>Choose Your Favorite Color</h1>
+    <div style={containerStyle}>
+      <h1 style={titleStyle}>Choose Your Favorite Color</h1>
       <select
-        className="form-select"
+        style={selectStyle}
         value={color}
         onChange={(e) => setColor(e.target.value)}
       >
@@ -21,12 +84,14 @@ export default function Color() {
         <option value="black">Black</option>
         <option value="white">White</option>
       </select>
-      <Link href="/">
-        <button className="btn btn-secondary mt-3">Back</button>
-      </Link>
-      <Link href={`/summary?age=${age}&color=${color}`}>
-        <button className="btn btn-primary mt-3 ms-2">Next</button>
-      </Link>
+      <div style={buttonContainerStyle}>
+        <Link href="/">
+          <button style={backButtonStyle}>Back</button>
+        </Link>
+        <Link href={`/summary?age=${age}&color=${color}`}>
+          <button style={nextButtonStyle}>Next</button>
+        </Link>
+      </div>
     </div>
   );
 }
--- src/pages/summary.js
+++ src/pages/summary.js
@@ -5,13 +5,45 @@ export default function Summary() {
   const router = useRouter();
   const { age, color } = router.query;
 
+  const containerStyle = {
+    paddingTop: "50px",
+    backgroundColor: "#F0F4F8", // light grey background
+    minHeight: "100vh",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+  };
+
+  const textStyle = {
+    color: "#333333", // dark text for readability
+    marginBottom: "0.5rem", // gives some space between the text lines
+  };
+
+  const buttonStyle = {
+    backgroundColor: "#6c757d", // a standard bootstrap secondary button color
+    color: "#FFFFFF", // white text for contrast
+    padding: "10px 15px",
+    borderRadius: "5px",
+    boxShadow: "0 2px 4px rgba(0, 0, 0, 0.2)", // button shadow for depth
+    cursor: "pointer",
+    textDecoration: "none",
+    display: "block", // for full width
+    width: "100%", // full width of the parent container
+    maxWidth: "500px", // max-width of the button
+    textAlign: "center",
+    border: "none",
+    marginTop: "20px", // space above the button
+  };
+
   return (
-    <div className="container">
-      <h1>Summary</h1>
-      <p>Age: {age}</p>
-      <p>Favorite Color: {color}</p>
+    <div style={containerStyle}>
+      <h1 style={{ ...textStyle, marginBottom: "1rem" }}>Summary</h1>{" "}
+      {/* More margin for the heading */}
+      <p style={textStyle}>Age: {age}</p>
+      <p style={textStyle}>Favorite Color: {color}</p>
       <Link href={`/color?age=${age}`}>
-        <button className="btn btn-secondary">Back</button>
+        <button style={buttonStyle}>Back</button>
       </Link>
     </div>
   );
